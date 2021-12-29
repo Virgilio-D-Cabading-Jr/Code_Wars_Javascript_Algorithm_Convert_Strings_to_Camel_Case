ca3c3aaa9a4aa418f29a4a9e55bd200768739e9c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-29 10:36:28 -0600

    detect new word after - or _

--- algo.js
+++ algo.js
@@ -8,6 +8,12 @@ function toCamelCase(str){
     // Loop through each character of string str
     for (let idx=0; idx < str.length; idx++) {
         console.log(str[idx]);
+        if (str[idx-1] == '-' || str[idx-1] == '_') {
+            console.log("new word found");
+            output += str[idx].toUpperCase();
+        } else {
+            output += str[idx];
+        }
     }
 
     return output;
