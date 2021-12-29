0c52e433fb4db38dff03715987a141f3a5684b41
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-29 10:38:22 -0600

    remove debug bonsole logs

--- algo.js
+++ algo.js
@@ -7,9 +7,7 @@ function toCamelCase(str){
 
     // Loop through each character of string str
     for (let idx=0; idx < str.length; idx++) {
-        console.log(str[idx]);
         if (str[idx-1] == '-' || str[idx-1] == '_') {
-            console.log("new word found");
             output += str[idx].toUpperCase();
         } else {
             output += str[idx];
